84f932a64f5efffa367edf36a7c73d953a2ba34c
Nathan <n8benzor@gmail.com>
2019-06-08 21:03:03 -0700

    Updated files

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Generate a random numer between 19-120 so the game can begin
+let computerNumber = Math.floor(Math.random() * 120 + 19);
+console.log(computerNumber);
+$('#random-number').html(computerNumber);
+
+// Genreate on click events for each image to generate a random number between 1-12 for all 4 images
+// $('#image-1').on('click', function(){
+
+// }
+
+
+
+
+
+// )
